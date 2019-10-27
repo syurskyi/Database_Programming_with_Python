ff2290ce009af3b9defc7272e1d51e5abe9623fe
Grey <sergejyurskyj@yahoo.com>
2019-10-27 01:38:05 -0700

    Commit Section 2: Creating Databases and Tables Update Bad update

--- Section 4 Python and Relational Databases/mysql.py
+++ Section 4 Python and Relational Databases/mysql.py
@@ -1,13 +1,13 @@
-import pyodbc
+# import pyodbc
+#
+# server = "localhost"
+# database = "vc-helpdesk"
+# username = "root"
+# password = ""
+# conn = pyodbc.connect("Login Prompt=False;DRIVER={Devart ODBC Driver for MySQL} ;SERVER="
+#                       + server +";DATABASE=" + database + ";UID=" + username + ";PWD=" + password)
+# if conn:
+#     conn.close()
 
-server = "localhost"
-database = "vc-helpdesk"
-username = "root"
-password = ""
-conn = pyodbc.connect("Login Prompt=False;DRIVER={MySQL ODBC 8.0 ANSI Driver}"
-                      ";SERVER=" + server +
-                      ";DATABASE=" + database +
-                      ";UID=" + username +
-                      ";PWD=" + password)
-if conn:
-    conn.close()
+
+# %windir%\system32\odbcad32.exe
--- Section 4 Python and Relational Databases/sqlserver.py
+++ Section 4 Python and Relational Databases/sqlserver.py
@@ -1,20 +1,20 @@
-import pyodbc
-
-# CONNECT TO DATABASE
-server = "webdb, 1400"
-database = "vc-helpdesk"
-username = "login_extracts"
-password = ""
-
-conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}"
-                      "; SERVER=" + server +
-                      ";DATABASE=" + database +
-                      ";UID=" + username +
-                      ";PWD=" + password)
-
-# ISSUE SELECT STATEMENTS
-
-# CLOSE CONNECTION
-if conn:
-    conn.close()
-
+# import pyodbc
+#
+# # CONNECT TO DATABASE
+# server = "webdb, 1400"
+# database = "vc-helpdesk"
+# username = "login_extracts"
+# password = ""
+#
+# conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}"
+#                       "; SERVER=" + server +
+#                       ";DATABASE=" + database +
+#                       ";UID=" + username +
+#                       ";PWD=" + password)
+#
+# # ISSUE SELECT STATEMENTS
+#
+# # CLOSE CONNECTION
+# if conn:
+#     conn.close()
+#
